c718ec14e46627c36e860c6e9286903bc088a200
= <=>
2024-03-13 13:46:51 +0100

    chore: change default port

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ import { routes } from './config/routes';
 
 dotenv.config()
 
-const PORT = process.env.PORT || 4000
+const PORT = process.env.PORT || 6000
 const app = express()
 
 setupProxies(app, routes)
